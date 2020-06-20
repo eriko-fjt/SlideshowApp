f202af070f2a4fd487301f5ee53bbd90f82b2f13
eriko-fjt <tangtin.waileizi@gmail.com>
2020-06-20 12:00:14 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 藤田恵梨子 on 2020/06/20.
+//  Copyright © 2020 eriko.fujita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
